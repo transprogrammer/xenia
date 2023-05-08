5b9bd3cc5ea3a1b1798b5b38c09cd0553d8bcb76
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-05-08 05:30:50 -0500

    config checkpoint

--- src/internal/config/config.go
+++ src/internal/config/config.go
@@ -4,62 +4,60 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
-
-	"github.com/aws/jsii-runtime-go"
 )
 
 const ConfigFile = "config.json"
 
-type ConfigJSON struct {
-	SubscriptionId  string            `json:"subscriptionId"`
-	ProjectName     string            `json:"projectName"`
-	PrimaryRegion   string            `json:"primaryRegion"`
-	SecondaryRegion string            `json:"secondaryRegion"`
-	AddressSpace    []string          `json:"addressSpace"`
-	Subnets         map[string]string `json:"subnets"`
+type regions struct {
+	primary   *string `json:"primary"`
+	secondary *string `json:"secondary"`
+}
+
+type subnets struct {
+	virtualMachine *string `json:"virtualMachine"`
+	mongoDB        *string `json:"mongoDB"`
+}
+
+type imageReference struct {
+	publisher *string `json:"publisher"`
+	offer     *string `json:"offer"`
+	sku       *string `json:"sku"`
+	version   *string `json:"version"`
+}
+
+type virtualMachine struct {
+	size               *string        `json:"size"`
+	storageAccountType *string        `json:"storageAccountType"`
+	imageReference     imageReference `json:"imageReference"`
+}
+
+type databaseAccount struct {
+	kind                    *string   `json:"kind"`
+	serverVersion           *string   `json:"serverVersion"`
+	offerType               *string   `json:"offerType"`
+	backupPolicyType        *string   `json:"backupPolicyType"`
+	defaultConsistencyLevel *string   `json:"defaultConsistencyLevel"`
+	capabilities            []*string `json:"capabilities"`
 }
 
 type Config struct {
-	projectName     *string
-	subscriptionId  *string
-	primaryRegion   *string
-	secondaryRegion *string
-	addressSpace    []*string
-	subnets         map[*string]*string
-	ids             *ids
-}
-
-func sliceToJsii(strs []string) []*string {
-	jsiiStrs := make([]*string, len(strs))
-	for i, str := range strs {
-		jsiiStrs[i] = jsii.String(str)
-	}
-	return jsiiStrs
-}
-
-func mapToJsii(strMap map[string]string) map[*string]*string {
-	jsiiMap := make(map[*string]*string, len(strMap))
-	for k, v := range strMap {
-		jsiiMap[jsii.String(k)] = jsii.String(v)
-	}
-	return jsiiMap
+	subscriptionId *string        `json:"subscriptionId"`
+	projectName    *string        `json:"projectName"`
+	regions        *string        `json:"regions"`
+	addressSpace   []*string      `json:"addressSpace"`
+	subnets        subnets        `json:"subnets"`
+	virtualMachine virtualMachine `json:"virtualMachine"`
+	ids            *ids
 }
 
 func MakeConfig() *Config {
-	json := makeConfigJSON()
+	config := makeConfig()
+	config.ids = makeIds()
 
-	return &Config{
-		projectName:     jsii.String(json.ProjectName),
-		subscriptionId:  jsii.String(json.SubscriptionId),
-		primaryRegion:   jsii.String(json.PrimaryRegion),
-		secondaryRegion: jsii.String(json.SecondaryRegion),
-		addressSpace:    sliceToJsii(json.AddressSpace),
-		subnets:         mapToJsii(json.Subnets),
-		ids:             MakeIds(),
-	}
+	return config
 }
 
-func makeConfigJSON() ConfigJSON {
+func makeConfig() *Config {
 	file, err := os.Open(ConfigFile)
 	if err != nil {
 		panic(err)
@@ -71,39 +69,107 @@ func makeConfigJSON() ConfigJSON {
 		panic(err)
 	}
 
-	var jso ConfigJSON
-	err = json.Unmarshal(bytes, &jso)
+	var cfg Config
+	err = json.Unmarshal(bytes, &cfg)
 	if err != nil {
 		panic(err)
 	}
 
-	return jso
+	return &cfg
 }
 
-func (cfg Config) ProjectName() *string {
+func (cfg Config) projectName() *string {
 	return cfg.projectName
 }
 
-func (cfg Config) SubscriptionId() *string {
+func (cfg Config) subscriptionId() *string {
 	return cfg.subscriptionId
 }
 
-func (cfg Config) PrimaryRegion() *string {
-	return cfg.primaryRegion
-}
-
-func (cfg Config) SecondaryRegion() *string {
-	return cfg.secondaryRegion
-}
-
-func (cfg Config) AddressSpace() []*string {
+func (cfg Config) addressSpace() []*string {
 	return cfg.addressSpace
 }
 
-func (cfg Config) Subnets() map[*string]*string {
+func (cfg Config) subnets() map[*string]*string {
 	return cfg.subnets
+}
+
+func (cfg Config) virtualMachine() *virtualMachine {
+	return cfg.virtualMachine
+}
+
+func (cfg Config) databaseAccount() *databaseAccount {
+	return cfg.databaseAccount
 }
 
 func (cfg Config) Ids() *ids {
 	return cfg.ids
+}
+
+func (rgn regions) Primary() *string {
+	return rgn.primary
+}
+
+func (rgn regions) Secondary() *string {
+	return rgn.secondary
+}
+
+func (sbn subnets) VirtualMachine() *string {
+	return sbn.virtualMachine
+}
+
+func (sbn subnets) MongoDB() *string {
+	return sbn.mongoDB
+}
+
+func (img imageReference) Publisher() *string {
+	return img.publisher
+}
+
+func (img imageReference) Offer() *string {
+	return img.offer
+}
+
+func (img imageReference) Sku() *string {
+	return img.sku
+}
+
+func (img imageReference) Version() *string {
+	return img.version
+}
+
+func (vm virtualMachine) Size() *string {
+	return vm.size
+}
+
+func (vm virtualMachine) StorageAccountType() *string {
+	return vm.storageAccountType
+}
+
+func (vm virtualMachine) ImageReference() *imageReference {
+	return vm.imageReference
+}
+
+func (db databaseAccount) Kind() *string {
+	return db.kind
+}
+
+func (db databaseAccount) ServerVersion() *string {
+	return db.serverVersion
+}
+
+func (db databaseAccount) OfferType() *string {
+	return db.offerType
+}
+
+func (db databaseAccount) BackupPolicyType() *string {
+	return db.backupPolicyType
+}
+
+func (db databaseAccount) DefaultConsistencyLevel() *string {
+	return db.defaultConsistencyLevel
+}
+
+func (db databaseAccount) Capabilities() []*string {
+	return db.capabilities
 }
