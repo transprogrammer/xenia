3187aca7361052155aec4eec7c9bf91e5b454f7a
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-05-17 04:56:39 -0500

    go build

--- src/config.go
+++ src/config.go
@@ -63,18 +63,23 @@ type Config struct {
 }
 
 type Id struct {
-	AzureRMProvider                *string
-	NamingModule                   *string
-	ResourceGroup                  *string
-	VirtualNetwork                 *string
-	Subnet                         *string
-	PublicIPAddress                *string
-	NetworkInterface               *string
-	NetworkInterfaceNSGAssociation *string
-	NetworkInterfaceASGAssociation *string
-	NetworkSecurityGroup           *string
-	ApplicationSecurityGroup       *string
-	VirtualMachine                 *string
+	ApplicationSecurityGroup         *string
+	AzureRMProvider                  *string
+	CosmosDBAccount                  *string
+	NamingModule                     *string
+	NetworkInterface                 *string
+	NetworkInterfaceASGAssociation   *string
+	NetworkInterfaceNSGAssociation   *string
+	NetworkSecurityGroup             *string
+	PrivateDNSZone                   *string
+	PrivateDNSZoneGroup              *string
+	PrivateDNSZoneVirtualNetworkLink *string
+	PrivateEndpoint                  *string
+	PublicIPAddress                  *string
+	ResourceGroup                    *string
+	Subnet                           *string
+	VirtualMachine                   *string
+	VirtualNetwork                   *string
 }
 
 func makeConfig() Config {
@@ -101,17 +106,23 @@ func makeConfig() Config {
 
 func makeIds() Id {
 	return Id{
-		AzureRMProvider:                jsii.String("azurerm"),
-		NamingModule:                   jsii.String("naming"),
-		ResourceGroup:                  jsii.String("resource_group"),
-		VirtualNetwork:                 jsii.String("virtual_network"),
-		Subnet:                         jsii.String("subnet"),
-		PublicIPAddress:                jsii.String("public_ip_address"),
-		NetworkInterface:               jsii.String("network_interface"),
-		NetworkInterfaceNSGAssociation: jsii.String("network_interface_nsg_association"),
-		NetworkInterfaceASGAssociation: jsii.String("network_interface_asg_association"),
-		NetworkSecurityGroup:           jsii.String("network_security_group"),
-		ApplicationSecurityGroup:       jsii.String("application_security_group"),
-		VirtualMachine:                 jsii.String("virtual_machine"),
+		ApplicationSecurityGroup:         jsii.String("application_security_group"),
+		AzureRMProvider:                  jsii.String("azurerm"),
+		CosmosDBAccount:                  jsii.String("cosmosdb_account"),
+		NamingModule:                     jsii.String("naming"),
+		NetworkInterface:                 jsii.String("network_interface"),
+		NetworkInterfaceASGAssociation:   jsii.String("network_interface_asg_association"),
+		NetworkInterfaceNSGAssociation:   jsii.String("network_interface_nsg_association"),
+		NetworkSecurityGroup:             jsii.String("network_security_group"),
+		PrivateDNSZone:                   jsii.String("private_dns_zone"),
+		PrivateDNSZoneGroup:              jsii.String("private_dns_zone_group"),
+		PrivateDNSZoneVirtualNetworkLink: jsii.String("private_dns_zone_virtual_network_link"),
+		PrivateEndpoint:                  jsii.String("private_endpoint"),
+		PublicIPAddress:                  jsii.String("public_ip_address"),
+		ResourceGroup:                    jsii.String("resource_group"),
+		Subnet:                           jsii.String("subnet"),
+		VirtualMachine:                   jsii.String("virtual_machine"),
+		VirtualNetwork:                   jsii.String("virtual_network"),
 	}
+
 }
--- src/main.go
+++ src/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	ii "github.com/aws/jsii-runtime-go"
 	tf "github.com/hashicorp/terraform-cdk-go/cdktf"
 	"github.com/transprogrammer/xenia/generated/naming"
 
 	asg "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/applicationsecuritygroup"
+	dbacct "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/cosmosdbaccount"
 	nic "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/networkinterface"
 	nicasg "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/networkinterfaceapplicationsecuritygroupassociation"
 	nicnsg "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/networkinterfacesecuritygroupassociation"
 	nsg "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/networksecuritygroup"
+	pdnsz "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/privatednszone"
+	pdnszvnl "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/privatednszonevirtualnetworklink"
+	pe "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/privateendpoint"
 	prov "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/provider"
 	ip "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/publicip"
 	rg "github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v5/resourcegroup"
@@ -29,6 +35,7 @@ func makeNamingModule(suffixes []*string) naming.Naming {
 }
 
 var Nme naming.Naming = makeNamingModule([]*string{})
+var MongoNaming naming.Naming = makeNamingModule([]*string{ii.String("mongo")})
 
 var Rg rg.ResourceGroup = rg.NewResourceGroup(Stk, Ids.ResourceGroup,
 	&rg.ResourceGroupConfig{
@@ -156,6 +163,63 @@ var VM vm.VirtualMachine = vm.NewVirtualMachine(Stk, Ids.VirtualMachine, &vm.Vir
 			KeyData: Cfg.VirtualMachine.SSHPublicKey,
 		},
 	},
+})
+
+var dbAccount dbacct.CosmosdbAccount = dbacct.NewCosmosdbAccount(Stk, Ids.CosmosDBAccount, &dbacct.CosmosdbAccountConfig{
+	Name:                       Nme.CosmosdbAccountOutput(),
+	Location:                   Cfg.Regions.Primary,
+	ResourceGroupName:          Rg.Name(),
+	Kind:                       ii.String("MongoDB"),
+	OfferType:                  ii.String("Standard"),
+	MongoServerVersion:         ii.String("4.2"),
+	PublicNetworkAccessEnabled: ii.Bool(false),
+	ConsistencyPolicy: &dbacct.CosmosdbAccountConsistencyPolicy{
+		ConsistencyLevel: ii.String("Eventual"),
+	},
+	GeoLocation: &[]*dbacct.CosmosdbAccountGeoLocation{
+		{
+			Location:         Cfg.Regions.Secondary,
+			FailoverPriority: ii.Number(0),
+			ZoneRedundant:    ii.Bool(false),
+		},
+	},
+	Capabilities: &[]*dbacct.CosmosdbAccountCapabilities{
+		{
+			Name: ii.String("DisabledRateLimitingResponses"),
+		},
+		{
+			Name: ii.String("EnableServerless"),
+		},
+	},
+})
+
+// ???: Add database? <>
+// TODO: Add sg assocs <>
+// TODO: Hardening <>
+
+var PrivateEndpoint pe.PrivateEndpoint = pe.NewPrivateEndpoint(Stk, Ids.PrivateEndpoint, &pe.PrivateEndpointConfig{
+	Name:              Nme.PrivateEndpointOutput(),
+	Location:          Cfg.Regions.Primary,
+	ResourceGroupName: Rg.Name(),
+	SubnetId:          MongoDBSubnet.Id(),
+	PrivateServiceConnection: &pe.PrivateEndpointPrivateServiceConnection{
+		Name:                        ii.String("cosmosdb"),
+		PrivateConnectionResourceId: dbAccount.Id(),
+		SubresourceNames:            &[]*string{ii.String("MongoDB")},
+	},
+})
+
+var PrivateDNSZone pdnsz.PrivateDnsZone = pdnsz.NewPrivateDnsZone(Stk, Ids.PrivateDNSZone, &pdnsz.PrivateDnsZoneConfig{
+	Name:              ii.String("privatelink.mongo.cosmos.azure.com"),
+	ResourceGroupName: Rg.Name(),
+})
+
+var PrivateDNSZoneVirtualNetworkLink pdnszvnl.PrivateDnsZoneVirtualNetworkLink = pdnszvnl.NewPrivateDnsZoneVirtualNetworkLink(Stk, Ids.PrivateDNSZoneVirtualNetworkLink, &pdnszvnl.PrivateDnsZoneVirtualNetworkLinkConfig{
+	Name:                ii.String(fmt.Sprintf("%s-vnetlink", *MongoNaming.PrivateDnsZoneOutput())),
+	ResourceGroupName:   Rg.Name(),
+	PrivateDnsZoneName:  PrivateDNSZone.Name(),
+	VirtualNetworkId:    VNet.Id(),
+	RegistrationEnabled: ii.Bool(true),
 })
 
 func main() {
